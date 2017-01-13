5f65aa3ae76af321bbd2f9c59554451cd0b344e2
Kari Bancroft <kariabancroft@gmail.com>
2017-01-12 21:43:40 -0800

    Fix issue with the repo page url not actually linking to anything

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,7 +2,13 @@ class Repo < ActiveRecord::Base
   has_and_belongs_to_many :cohorts
   has_many :submissions
 
+  BASE_GITHUB_URL = "http://github.com/"
+
   def pulls_url
-    "http://github.com/" + self.repo_url + "/pulls"
+    BASE_GITHUB_URL + self.repo_url + "/pulls"
+  end
+
+  def full_repo_url
+    BASE_GITHUB_URL + self.repo_url
   end
 end
