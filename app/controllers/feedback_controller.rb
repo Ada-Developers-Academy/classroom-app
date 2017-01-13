3861218fd7a4fb595072b4ab8d85c55b118a8852
Kari Bancroft <kariabancroft@gmail.com>
2017-01-13 14:03:32 -0800

    Add feedback support. Pull from actual repo for feedback template. Does not update the submission entry yet with the URL.

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,7 +1,24 @@
+require 'github_comment'
+
 class FeedbackController < ApplicationController
   def new
+    # Get the feedback template
+    @repo = Repo.find(params[:repo_id])
+    @feedback_template = GitHubComment.find_template(@repo)
+    @student_name = Student.find(params[:student_id]).name
+    @submission = Submission.find_by(student: params[:student_id], repo: params[:repo_id])
   end
 
   def create
+    submit = Submission.find_by(student: params[:student_id], repo: params[:repo_id])
+    if !submit
+      render :new
+    else
+        feedback_url = GitHubComment.add_new(params[:Feedback], submit.repo.repo_url, submit.pr_id)
+        p response
+        # Update the submission
+        # submit.feedback_url = response[:url]
+        redirect_to repo_cohort_path(params[:repo_id])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
 
   resources :repos do
     get "/cohort/:cohort_id", to: "repos#show", as: :cohort
+    get "/student/:student_id/feedback/new", to: "feedback#new", as: :new_feedback
+    post "/student/:student_id/feedback/", to: "feedback#create", as: :feedback
+    resources :students do
+      resources :feedback
+    end
   end
   resources :students
   resources :pull_requests
+
 
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
