2a36fc2cacda514ed0e7f2c7b4efaa3d74b79ba4
Charles Ellis <hamled@hamled.name>
2017-01-25 14:49:39 -0800

    FeedbackController shared model-finding logic

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,10 +1,10 @@
 require 'github_comment'
 
 class FeedbackController < ApplicationController
+  before_action :find_objects
+
   def new
-    @repo = Repo.find(params[:repo_id])
     @feedback_template = GitHubComment.find_template(@repo)
-    @submission = Submission.find_by(student: params[:student_id], repo: params[:repo_id])
 
     # Group or individual?
     if @repo.individual
@@ -17,25 +17,30 @@ class FeedbackController < ApplicationController
   end
 
   def create
-    submission = Submission.find_by(student: params[:student_id], repo: params[:repo_id])
-    if !submission
+    if !@submission
       render :new
     else
-      feedback_url = GitHubComment.add_new(params[:Feedback], submission.repo.repo_url, submission.pr_id)
+      feedback_url = GitHubComment.add_new(params[:Feedback], @repo.repo_url, @submission.pr_id)
 
       # Check if group
-      repo = Repo.find(params[:repo_id])
-      if !repo.individual
-        submission_list = submission.find_shared
+      if !@repo.individual
+        submission_list = @submission.find_shared
       else
         submission_list = []
-        submission_list << submission
+        submission_list << @submission
       end
 
       # Update the submissions
       Submission.update_many(submission_list, feedback_url)
 
-      redirect_to repo_cohort_path(repo, submission.student.cohort)
+      redirect_to repo_cohort_path(@repo, @submission.student.cohort)
     end
+  end
+
+  private
+
+  def find_objects
+    @repo = Repo.find(params[:repo_id])
+    @submission = Submission.find_by(student: params[:student_id], repo: @repo)
   end
 end
