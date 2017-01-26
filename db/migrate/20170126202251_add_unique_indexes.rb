07b4bc988185e8fed854e0b9c31eea76df1d14ff
Charles Ellis <hamled@hamled.name>
2017-01-26 12:39:39 -0800

    Improve DB integrity with unique indexes
    This adds unique indexes on a number of tables:
    
    * `users` - OAuth identity fields should be unique as a pair
    * `students` - Only one of a GitHub account per cohort
    * `user_invites` - Only one invite for a given GitHub account
    * `cohorts` - Number & name combined should be unique
    * `cohorts_repos` - Join table between `repos` & `cohorts` is unique

---
+++ db/migrate/20170126202251_add_unique_indexes.rb
@@ -0,0 +1,11 @@
+class AddUniqueIndexes < ActiveRecord::Migration
+  def change
+    add_index :users, [:uid, :provider], unique: true
+
+    add_index :students, [:cohort_id, :github_name], unique: true
+    add_index :user_invites, :github_name, unique: true
+
+    add_index :cohorts_repos, [:cohort_id, :repo_id], unique: true
+    add_index :cohorts, [:number, :name], unique: true
+  end
+end
