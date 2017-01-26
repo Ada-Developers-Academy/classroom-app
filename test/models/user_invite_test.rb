f5f4581e5e394d5e5fe06a75276c28cfb76ee479
Charles Ellis <hamled@hamled.name>
2017-01-26 12:14:47 -0800

    Refactor test setup for UserInvite validations

--- test/models/user_invite_test.rb
+++ test/models/user_invite_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 
 class UserInviteTest < ActiveSupport::TestCase
   class Validations < UserInviteTest
-    test 'validates role is in predefined set' do
-      valid_invite = user_invites(:instructor)
+    def valid_invite
+      user_invites(:instructor)
+    end
+
+    setup do
       # sanity check
       assert valid_invite.valid?
+    end
 
+    test 'validates role is in predefined set' do
       User::ROLES.each do |role|
         valid_invite.role = role
         assert valid_invite.valid?
