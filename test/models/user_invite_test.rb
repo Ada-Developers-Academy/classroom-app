02a9edddb2d1b099ce2d84b576521c591e310e78
Charles Ellis <hamled@hamled.name>
2017-01-26 12:03:46 -0800

    UserInvite validates role attribute

--- test/models/user_invite_test.rb
+++ test/models/user_invite_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserInviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  class Validations < UserInviteTest
+    test 'validates role is in predefined set' do
+      valid_invite = user_invites(:instructor)
+      # sanity check
+      assert valid_invite.valid?
+
+      User::ROLES.each do |role|
+        valid_invite.role = role
+        assert valid_invite.valid?
+      end
+
+      invalid_role = User::ROLES.sum
+      valid_invite.role = invalid_role
+      refute valid_invite.valid?
+    end
+  end
 end
