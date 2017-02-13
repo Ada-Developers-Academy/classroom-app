adeaa8cdb09eb9d7b4184675b6cdc6ead14804af
Kari Bancroft <kariabancroft@gmail.com>
2017-02-13 13:05:31 -0800

    Move github comment methods to instance methods so we can set the token instance variable. Adjust the controller call to utilize the new github comment instance var

--- lib/github_comment.rb
+++ lib/github_comment.rb
@@ -2,16 +2,21 @@ require 'httparty'
 require 'pr_student'
 
 class GitHubComment
-  AUTH = { :username => ENV["GITHUB"] }
   # def get_comments()
   #   "https://api.github.com/repos/#{ repo_url }/pulls"
   #   response = HTTParty.get(url, headers: {"user-agent" => "rails"}, :basic_auth => GitHubComment::AUTH)
   #   return response
   # end
 
-  def self.find_template(repo)
+  attr_reader :token
+
+  def initialize(token)
+    @token = token
+  end
+
+  def find_template(repo)
     url = "https://raw.githubusercontent.com/#{ repo.repo_url }/master/feedback.md"
-    response = HTTParty.get(url, headers: {"user-agent" => "rails"}, :basic_auth => GitHubComment::AUTH)
+    response = HTTParty.get(url, headers: {"user-agent" => "rails", "Authorization" => "token #{ @token }"})
     if response.code == 404
       return nil
     else
@@ -19,14 +24,15 @@ class GitHubComment
     end
   end
 
-  def self.add_new(comment, repo_url, pr_id)
+  def add_new(comment, repo_url, pr_id)
     url = "https://api.github.com/repos/#{ repo_url }/issues/#{ pr_id }/comments"
-    # p "URLURLURL #{url} and the comment #{ comment }"
+
     response = HTTParty.post(url,
       :body => {:body => comment }.to_json,
-      headers: {"user-agent" => "rails", "content-type" => "application/json"},
-      :basic_auth => GitHubComment::AUTH)
-    # p result
+      headers: {"user-agent" => "rails", "content-type" => "application/json", "Authorization" => "token #{ @token }"})
+    puts "<" * 40
+    puts response
+    puts response.code
     if response.code == 201
       feedback_url = response.parsed_response["html_url"]
       return feedback_url
