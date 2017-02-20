3bb25140070e109bbdc8ea6a774b11a2c5baceb3
Kari Bancroft <kariabancroft@gmail.com>
2017-02-20 11:01:26 -0800

    Add testing for student edit and update

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
   setup do
-    session[:user_id] = User.first.id
+    session[:user_id] = users(:instructor).id
+    @student = students(:shark)
   end
 
   test "should get the new form" do
@@ -26,4 +27,23 @@ class StudentsControllerTest < ActionController::TestCase
     assert_redirected_to students_path
   end
 
+  test "should get the edit form" do
+    get :edit, { id: @student.id }
+
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "should not update a student without required fields" do
+    patch :update, { id: @student.id , student: { name: nil }}
+
+    assert_response :bad_request
+    assert_template :edit
+  end
+
+  test "should redirect when updated successfully" do
+    patch :update, { id: @student.id, student: { github_name: "othername" }}
+
+    assert_redirected_to students_path
+  end
 end
