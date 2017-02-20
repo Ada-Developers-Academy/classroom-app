a2098dd0c866645e44bfb8fd9f0acef9f04727fa
Kari Bancroft <kariabancroft@gmail.com>
2017-02-20 11:32:06 -0800

    Get started with repo tests

--- test/controllers/repos_controller_test.rb
+++ test/controllers/repos_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ReposControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    session[:user_id] = users(:instructor).id
+  end
+
+  test "index should load list of repos" do
+    get :index
+
+    assert_template :index
+    assert_response :success
+  end
+
+  test "should get the new form" do
+    get :new
+
+    assert_response :success
+    assert_template :new
+  end
 end
