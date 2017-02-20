2dd1b853f126a9ebc2b1882770c60add8d61f147
Kari Bancroft <kariabancroft@gmail.com>
2017-02-20 10:41:58 -0800

    Add initial test set for student creation

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    session[:user_id] = User.first.id
+  end
+
+  test "should get the new form" do
+    get :new
+
+    assert_response :success
+    assert_template :new
+  end
+
+  test "should not create a student without required fields" do
+    post :create, { student: { name: "test" }}
+
+    assert_response :bad_request
+    assert_template :new
+  end
+
+  test "should redirect when created successfully with required fields" do
+    post :create, { student: { name: "test", github_name: "test_github",
+      email: "test@github.com", cohort_id: Cohort.first.id }}
+
+    assert_redirected_to students_path
+  end
+
 end
