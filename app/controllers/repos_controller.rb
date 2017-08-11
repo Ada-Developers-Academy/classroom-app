a5d70bff2df62bca65e8796e83ecd91b3970c780
Charles Ellis <hamled@hamled.name>
2017-08-11 15:51:05 -0700

    Verify authorization in ReposController

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,18 +1,12 @@
 require 'github'
 
 class ReposController < ApplicationController
+  load_and_authorize_resource
+
   def index
-    @repos = Repo.all
   end
 
   def show
-    @repo = Repo.find_by_id(params[:repo_id])
-
-    if !@repo
-      flash[:error] = "Repository not found"
-      redirect_to :back
-    end
-
     @cohort = Cohort.find_by_id(params[:cohort_id])
     if !@cohort
       flash[:error] = "Cohort not found"
@@ -24,12 +18,9 @@ class ReposController < ApplicationController
   end
 
   def new
-    @repo = Repo.new
   end
 
   def create
-    @repo = Repo.new(repo_params)
-
     if @repo.save
       redirect_to repos_path
     else
@@ -38,16 +29,10 @@ class ReposController < ApplicationController
   end
 
   def edit
-    @repo = Repo.find_by_id(params[:id])
-    if !@repo
-      flash[:error] = "Repo not found."
-      redirect_to repos_path
-    end
     @max_size = Cohort.all.length
   end
 
   def update
-    @repo = Repo.find(params[:id])
     if @repo.update_attributes(repo_params)
       @repo.cohorts.build
       redirect_to repos_path
@@ -57,16 +42,16 @@ class ReposController < ApplicationController
   end
 
   def destroy
-    repo = Repo.find_by_id(params[:id])
-    if repo
-      repo.destroy
-    else
-      flash[:error] = "Repo not found."
-    end
+    @repo.destroy
     redirect_to repos_path
   end
 
   private
+
+  rescue_from ActiveRecord::RecordNotFound do |ex|
+    flash[:error] = "Repo not found."
+    redirect_to repos_path
+  end
 
   def repo_params
     params.require(:repo).permit(:repo_url, :individual, :cohort_ids => [] )
