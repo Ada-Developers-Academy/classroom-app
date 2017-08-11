9bc99a0077df4fbcae530493bb1c0ea7a2d3a5a9
Charles Ellis <hamled@hamled.name>
2017-08-11 16:34:28 -0700

    Verify authorization in StudentsController

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,10 +1,41 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
+  setup do
+    @student = students(:shark)
+  end
+
+  def create_params
+    {
+      student: {
+        name: "test",
+        github_name: "test_github",
+        email: "test@example.com",
+        cohort_id: cohorts(:jets).id
+      }
+    }
+  end
+
+  def create_params_invalid
+    create_params.deep_merge(student: { github_name: nil, email: nil, cohort_id: nil })
+  end
+
+  def update_params
+    {
+      id: @student.id,
+      student: {
+        github_name: "othername"
+      }
+    }
+  end
+
+  def update_params_invalid
+    update_params.deep_merge(student: { github_name: nil })
+  end
+
   class Functionality < StudentsControllerTest
     setup do
       session[:user_id] = users(:instructor).id
-      @student = students(:shark)
     end
 
     test "should get the new form" do
@@ -15,15 +46,14 @@ class StudentsControllerTest < ActionController::TestCase
     end
 
     test "should not create a student without required fields" do
-      post :create, { student: { name: "test" }}
+      post :create, create_params_invalid
 
       assert_response :bad_request
       assert_template :new
     end
 
     test "should redirect when created successfully with required fields" do
-      post :create, { student: { name: "test", github_name: "test_github",
-        email: "test@github.com", cohort_id: cohorts(:jets).id }}
+      post :create, create_params
 
       assert_redirected_to students_path
     end
@@ -43,14 +73,14 @@ class StudentsControllerTest < ActionController::TestCase
     end
 
     test "should not update a student without required fields" do
-      patch :update, { id: @student.id , student: { name: nil }}
+      patch :update, update_params_invalid
 
       assert_response :bad_request
       assert_template :edit
     end
 
     test "should redirect when updated successfully" do
-      patch :update, { id: @student.id, student: { github_name: "othername" }}
+      patch :update, update_params
 
       assert_redirected_to students_path
     end
@@ -87,6 +117,259 @@ class StudentsControllerTest < ActionController::TestCase
 
       assert_template :index
       assert_response :success
+    end
+  end
+
+  class Authorization < StudentsControllerTest
+    class Instructor < Authorization
+      setup do
+        session[:user_id] = users(:instructor).id
+      end
+
+      test "should get index of students" do
+        get :index
+
+        assert_response :success
+      end
+
+      test "should get show page of student" do
+        get :show, id: @student.id
+
+        assert_response :success
+      end
+
+      test "should get new student form" do
+        get :new
+
+        assert_response :success
+      end
+
+      test "should create new student" do
+        post :create, create_params
+
+        assert_response :redirect
+        assert_redirected_to students_path
+      end
+
+      test "should get edit student form" do
+        get :edit, id: @student.id
+
+        assert_response :success
+      end
+
+      test "should update existing student" do
+        patch :update, update_params
+
+        assert_response :redirect
+        assert_redirected_to students_path
+      end
+
+      test "should destroy existing student" do
+        delete :destroy, id: @student.id
+
+        assert_redirected_to students_path
+      end
+    end
+
+    class Student < Authorization
+      setup do
+        session[:user_id] = users(:student).id
+      end
+
+      test "should not get index of students" do
+        get :index
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get show page of student" do
+        get :show, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get new student form" do
+        get :new
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not create new student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          post :create, create_params
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
+
+      test "should not get edit student form" do
+        get :edit, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not update existing student" do
+        patch :update, update_params
+
+        assert_not_equal @student.reload.github_name, "othername"
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not destroy existing student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          delete :destroy, id: @student.id
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
+    end
+
+    class Unauthorized < Authorization
+      setup do
+        session[:user_id] = users(:unknown).id
+      end
+
+      test "should not get index of students" do
+        get :index
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get show page of student" do
+        get :show, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get new student form" do
+        get :new
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not create new student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          post :create, create_params
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
+
+      test "should not get edit student form" do
+        get :edit, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not update existing student" do
+        patch :update, update_params
+
+        assert_not_equal @student.reload.github_name, "othername"
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not destroy existing student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          delete :destroy, id: @student.id
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
+    end
+
+    class Guest < Authorization
+      setup do
+        session[:user_id] = nil
+      end
+
+      test "should not get index of students" do
+        get :index
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get show page of student" do
+        get :show, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not get new student form" do
+        get :new
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not create new student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          post :create, create_params
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
+
+      test "should not get edit student form" do
+        get :edit, id: @student.id
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not update existing student" do
+        patch :update, update_params
+
+        assert_not_equal @student.reload.github_name, "othername"
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not destroy existing student" do
+        assert_no_difference(lambda { ::Student.count }) do
+          delete :destroy, id: @student.id
+
+          assert_response :redirect
+          assert_redirected_to root_path
+          assert_not_empty flash[:error]
+        end
+      end
     end
   end
 end
