d1f1488de93d66bb107f6e5d29be928141bb4442
Charles Ellis <hamled@hamled.name>
2017-08-11 14:39:05 -0700

    Controller tests for creating feedback

--- test/controllers/feedback_controller_test.rb
+++ test/controllers/feedback_controller_test.rb
@@ -13,16 +13,53 @@ class FeedbackControllerTest < ActionController::TestCase
     }
   end
 
+  def create_params
+    new_params.merge({
+      Feedback: "This is feedback for a project submission, used in our unit tests."
+    })
+  end
+
   class Functionality < FeedbackControllerTest
     setup do
       session[:user_id] = users(:instructor).id
     end
 
-    test "should get the new form" do
-      get :new, new_params
+    def feedback_url
+      "https://github.com/Ada-test/PR-App-test-individual/pull/1/#issue-1234567890"
+    end
 
-      assert_response :success
-      assert_template :new
+    def api_mock
+      Minitest::Mock.new.expect(:add_new, feedback_url, [String, String, String])
+    end
+
+    class New < Functionality
+      test "should get the new form" do
+        get :new, new_params
+
+        assert_response :success
+        assert_template :new
+      end
+    end
+
+    class Create < Functionality
+      test "should redirect to repo show page" do
+        GitHubComment.stub :new, api_mock do
+          post :create, create_params
+
+          assert_response :redirect
+          assert_redirected_to repo_cohort_path(@repo, @student.cohort)
+        end
+      end
+
+      test "should save the feedback comment URL for all grouped submissions" do
+        GitHubComment.stub :new, api_mock do
+          post :create, create_params
+
+          Submission.grouped_with(@student.submission_for_repo(@repo)).each do |sub|
+            assert sub.feedback_url = feedback_url
+          end
+        end
+      end
     end
   end
 
@@ -37,6 +74,13 @@ class FeedbackControllerTest < ActionController::TestCase
 
         assert_response :success
       end
+
+      test "should be able to create feedback" do
+        post :create, create_params
+
+        assert_response :redirect
+        assert_redirected_to repo_cohort_path(@repo, @student.cohort)
+      end
     end
 
     class Student < Authorization
@@ -46,6 +90,14 @@ class FeedbackControllerTest < ActionController::TestCase
 
       test "should not be able to get the new form" do
         get :new, new_params
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not be able to create feedback" do
+        post :create, create_params
 
         assert_response :redirect
         assert_redirected_to root_path
@@ -65,6 +117,14 @@ class FeedbackControllerTest < ActionController::TestCase
         assert_redirected_to root_path
         assert_not_empty flash[:error]
       end
+
+      test "should not be able to create feedback" do
+        post :create, create_params
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
     end
 
     class Guest < Authorization
@@ -74,6 +134,14 @@ class FeedbackControllerTest < ActionController::TestCase
 
       test "should not be able to get the new form" do
         get :new, new_params
+
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_not_empty flash[:error]
+      end
+
+      test "should not be able to create feedback" do
+        post :create, create_params
 
         assert_response :redirect
         assert_redirected_to root_path
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/mock'
 
 # Uncomment these lines to automatically start pry
 # when a test fails or an exception is unhandled.
