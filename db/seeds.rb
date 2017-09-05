fc24ebe8cb2d5be39afef11f0fdb201a5529346d
Charles Ellis <hamled@hamled.name>
2017-09-05 15:08:28 -0700

    Don't silently fail when seeding
    We now call `create!` instead of `create` everywhere when seeding. This
    means that if the call fails we will see it due to the unhandled
    exception. Previously we might fail to create a record and see no output
    to indicate that this happened.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Create instructor
-User.create(name: "Test Instructor 1", provider: :github, uid: "30710012", role: "instructor")
+User.create!(name: "Test Instructor 1", provider: :github, uid: "30710012", role: "instructor")
 
 # Create cohorts
 cohorts = [
@@ -8,13 +8,13 @@ cohorts = [
 ]
 
 cohorts.each do |c|
-  Cohort.create(c)
+  Cohort.create!(c)
 end
 
 # Create students
-Student.create(name: "Test Student 1", cohort: Cohort.first,
+Student.create!(name: "Test Student 1", cohort: Cohort.first,
                github_name: "ada-student-1", email: "charles+classroom-local-student-1@adadev.org")
-Student.create(name: "Test Student 2", cohort: Cohort.last,
+Student.create!(name: "Test Student 2", cohort: Cohort.last,
                github_name: "ada-student-2", email: "charles+classroom-local-student-2@adadev.org")
 
 repos = [
@@ -23,7 +23,7 @@ repos = [
 ]
 
 repos.each do |repo|
-  r = Repo.create(repo)
+  r = Repo.create!(repo)
   r.cohorts << Cohort.all
   r.save
 end
