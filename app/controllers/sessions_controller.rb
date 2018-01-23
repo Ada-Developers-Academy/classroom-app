500389c52ef86df655548adb9f4759bbe48d4cd8
Kari Bancroft <kariabancroft@gmail.com>
2018-01-23 15:13:17 -0800

    updates to try to see if this works but pg is giving me issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,29 +5,34 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    if auth_hash && auth_hash["uid"]
-      @user = User.update_or_create_from_omniauth(auth_hash)
-      if @user
-        session[:user_id] = @user.id
-        session[:token] = auth_hash['credentials'].token
+    if params[:provider] != "schoology"
+      # github 
+      if auth_hash && auth_hash["uid"]
+        @user = User.update_or_create_from_omniauth(auth_hash)
+        if @user
+          session[:user_id] = @user.id
+          session[:token] = auth_hash['credentials'].token
 
-        # If they've been invited, accept it automatically
-        github_name = auth_hash["extra"]["raw_info"]["login"]
-        invite = UserInvite.acceptable.find_by(github_name: github_name)
-        if invite
-          begin
-            @user.accept_invite(invite)
-          rescue
-            return redirect_to root_path, notice: "Unable to accept invitation"
+          # If they've been invited, accept it automatically
+          github_name = auth_hash["extra"]["raw_info"]["login"]
+          invite = UserInvite.acceptable.find_by(github_name: github_name)
+          if invite
+            begin
+              @user.accept_invite(invite)
+            rescue
+              return redirect_to root_path, notice: "Unable to accept invitation"
+            end
           end
-        end
 
-        redirect_to root_path
+          redirect_to root_path
+        else
+          redirect_to root_path, notice: "Failed to save the user"
+        end
       else
-        redirect_to root_path, notice: "Failed to save the user"
+        redirect_to root_path, notice: "Failed to authenticate"
       end
     else
-      redirect_to root_path, notice: "Failed to authenticate"
+      # schoology
     end
   end
 
@@ -35,5 +40,12 @@ class SessionsController < ApplicationController
     session.delete :user_id
     session.delete :token
     redirect_to root_path
+  end
+
+  def github_auth
+
+  end
+
+  def schoology_auth
   end
 end
