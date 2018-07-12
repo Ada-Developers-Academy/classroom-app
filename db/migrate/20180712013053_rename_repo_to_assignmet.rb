ec5d14a370e4b141be7fcd28bdb127be2205e4ab
LetiTran <leticiastran@gmail.com>
2018-07-11 18:51:38 -0700

    Migrates to change Repo model name to Assignment

---
+++ db/migrate/20180712013053_rename_repo_to_assignmet.rb
@@ -0,0 +1,17 @@
+class RenameRepoToAssignmet < ActiveRecord::Migration[5.2]
+  def change
+
+    remove_index :submissions, [:student_id, :repo_id]
+    remove_index :cohorts_repos, [:cohort_id, :repo_id]
+
+    rename_table :repos, :assignments
+    rename_table :cohorts_repos, :cohorts_assignments
+
+    rename_column :cohorts_assignments, :repo_id, :assignment_id
+    rename_column :submissions, :repo_id, :assignment_id
+
+    add_index :submissions, [:student_id, :assignment_id], unique: true
+    add_index :cohorts_assignments, [:cohort_id, :assignment_id], unique: true
+      
+  end
+end
