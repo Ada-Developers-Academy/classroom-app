e1b988c628eca1d8f47aa07f72b1bd76e5129e95
LetiTran <leticiastran@gmail.com>
2018-07-13 11:50:36 -0700

    Commented out the admin 'full permission' user setting back to the real line of code to check session user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   check_authorization
 
   def current_user
-    # @user ||= User.find_by(id: session[:user_id])
+    @user ||= User.find_by(id: session[:user_id])
 
     # enable this for admin permission:
-    @user ||= User.find_by(id: 1)
+    # @user ||= User.find_by(id: 1)
     # dont forget to change it back to line 10 !
    
 
